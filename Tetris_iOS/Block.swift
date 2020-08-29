043301f046ffb809c9efd842efd6aeca8f7cc0b5
Ryan Leung <ryanleung1004@gmail.com>
2020-08-29 14:53:38 -0700

    Implemented a Block object

---
+++ Tetris_iOS/Block.swift
@@ -0,0 +1,71 @@
+//
+//  Block.swift
+//  Tetris_iOS
+//
+//  Created by Ryan Leung on 2020-08-29.
+//  Copyright © 2020 Ryan Leung. All rights reserved.
+//
+
+import SpriteKit
+
+let NumberOfColors: UInt32 = 6
+
+enum BlockColor: Int, CustomStringConvertible {
+    case Blue = 0, Orange, Purple, Red, Teal, Yellow
+    
+    var spriteName: String {
+        switch self {
+        case .Blue:
+            return "blue"
+        case .Orange:
+            return "orange"
+        case .Purple:
+            return "Purple"
+        case .Red:
+            return "red"
+        case .Teal:
+            return "teal"
+        case .Yellow:
+            return "yellow"
+        }
+    }
+    
+    var description: String {
+        return self.spriteName
+    }
+    
+    static func random() -> BlockColor {
+        return BlockColor(rawValue:Int(arc4random_uniform(NumberOfColors)))!
+    }
+}
+
+class Block: Hashable, CustomStringConvertible {
+    let color: BlockColor
+
+    var column: Int
+    var row: Int
+    var sprite: SKSpriteNode?
+    
+    var spriteName: String {
+        return color.spriteName
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(column)
+        hasher.combine(row)
+    }
+    
+    var description: String {
+        return "\(color): [\(column), \(row)]"
+    }
+    
+    init(column:Int, row:Int, color:BlockColor) {
+        self.column = column
+        self.row = row
+        self.color = color
+    }
+}
+
+func==(lhs: Block, rhs: Block) -> Bool {
+    return lhs.column == rhs.column && lhs.row == rhs.row && lhs.color.rawValue == rhs.color.rawValue
+}
