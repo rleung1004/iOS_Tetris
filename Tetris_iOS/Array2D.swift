0bf7284960f30a10c24cfa59cbc8737771c8c542
Ryan Leung <ryanleung1004@gmail.com>
2020-08-29 14:25:39 -0700

    Created Array2D

---
+++ Tetris_iOS/Array2D.swift
@@ -0,0 +1,29 @@
+//
+//  Array2D.swift
+//  Tetris_iOS
+//
+//  Created by Ryan Leung on 2020-08-29.
+//  Copyright © 2020 Ryan Leung. All rights reserved.
+//
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        
+        array = Array<T?>(repeating: nil, count:rows * columns)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
